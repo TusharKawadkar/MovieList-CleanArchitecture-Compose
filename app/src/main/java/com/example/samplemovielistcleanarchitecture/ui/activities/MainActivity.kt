5f942fcff1b99ebc6b0b7ed471a796873de913f2
Tushar Kawadkar <kawadkartushartech@gmail.com>
2023-12-20 18:52:24 +0530

    cleanup

--- app/src/main/java/com/example/samplemovielistcleanarchitecture/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/samplemovielistcleanarchitecture/ui/activities/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BottomNavigation
+import androidx.compose.material.Text
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -37,9 +39,11 @@ fun ApplicationScreen() {
         NavHost(
             navController = navigationController,
             startDestination = "",
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
         ) {
-
+            
         }
     }
 }
